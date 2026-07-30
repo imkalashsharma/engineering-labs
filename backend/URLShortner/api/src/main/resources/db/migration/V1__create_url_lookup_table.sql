8ed85a082bd5b06bfbed3fb7b411215100d4f4e0
Kalash Sharma <kalash.strt@gmail.com>
2026-07-31 04:24:48 +0530

    add: indexes on original url and short code

--- backend/URLShortner/api/src/main/resources/db/migration/V1__create_url_lookup_table.sql
+++ backend/URLShortner/api/src/main/resources/db/migration/V1__create_url_lookup_table.sql
@@ -1,3 +1,5 @@
+-- V1_create_url_lookup_table.sql
+
 CREATE TABLE url_lookup (
                             id BIGSERIAL PRIMARY KEY,
                             original_url TEXT NOT NULL,
@@ -5,3 +7,9 @@ CREATE TABLE url_lookup (
                             created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
                             updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE UNIQUE INDEX uk_url_lookup_short_code
+    ON url_lookup(short_code);
+
+CREATE UNIQUE INDEX uk_url_lookup_original_url
+    ON url_lookup(original_url);
