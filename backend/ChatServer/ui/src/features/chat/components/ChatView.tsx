0d16e63b01602c0a430d50f7b9fec38a766c4867
Kalash Sharma <kalash.strt@gmail.com>
2026-09-07 23:46:53 +0530

    add: message and bubble component for showing messages in chat view

--- backend/ChatServer/ui/src/features/chat/components/ChatView.tsx
+++ backend/ChatServer/ui/src/features/chat/components/ChatView.tsx
@@ -1,12 +1,27 @@
+import { Bubble, BubbleContent } from "../../../components/ui/bubble";
+import { Message, MessageContent } from "../../../components/ui/message";
 import { useChatPanel } from "../context/ChatPanelContext";
 
 const ChatView = () => {
-  const { messages } = useChatPanel();
+  const { userId, messages } = useChatPanel();
 
   return (
-    <div className="chatView bg-neutral-300 rounded-md shadow-md h-100 overflow-y-auto p-2">
+    <div className="chatView bg-slate-200 rounded-md shadow-md h-100 overflow-y-auto p-2">
       {messages.map((msg) => (
-        <div key={msg.messageId}>{msg.content}</div>
+        <div className="my-2">
+          <Message
+            key={msg.messageId}
+            align={msg.senderId === userId ? "end" : "start"}
+          >
+            <MessageContent>
+              <Bubble
+                variant={msg.senderId === userId ? "default" : "secondary"}
+              >
+                <BubbleContent>{msg.content}</BubbleContent>
+              </Bubble>
+            </MessageContent>
+          </Message>
+        </div>
       ))}
     </div>
   );
