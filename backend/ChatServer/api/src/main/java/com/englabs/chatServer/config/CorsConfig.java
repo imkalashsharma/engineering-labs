4e0a9460aad6261f0df963df18922dc3983f655c
Kalash Sharma <kalash.strt@gmail.com>
2026-09-13 19:54:26 +0530

    fix: ui url in cors

--- backend/ChatServer/api/src/main/java/com/englabs/chatServer/config/CorsConfig.java
+++ backend/ChatServer/api/src/main/java/com/englabs/chatServer/config/CorsConfig.java
@@ -1,21 +1,24 @@
 package com.englabs.chatServer.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
+    @Value("${app.cors.allowed-origins}")
+    private List<String> allowedOrigins;
 
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(
-                java.util.List.of("http://localhost:5173")
-        );
+        config.setAllowedOrigins(allowedOrigins);
 
         config.setAllowedMethods(
                 java.util.List.of(
